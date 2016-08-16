ce5afd13c2df9f6f90041f16d63ebbc531a51b44
Dylan & Madeleine <pair+dylanhul.madeleine-rashbrooke@devbootcamp.com>
2016-08-16 21:19:23 +1200

    Split 3S data from main shape file, first static map URL generated

---
+++ dataByRoute/3S-map-v1.js
@@ -0,0 +1,2 @@
+//the url for a static image for part of the 3 bus route
+"https://api.mapbox.com/styles/v1/madeleine-rashbrooke/cirx8mls40000g9navn0bzs80/static/174.753609,-41.288874,12.50,0.00,0.00/600x400?access_token=pk.eyJ1IjoibWFkZWxlaW5lLXJhc2hicm9va2UiLCJhIjoiY2lyeDVqdXRmMDA4OTJ5b2ZxZXc1dDg5dCJ9.6kyfgf7klxvu7UizZNzA9A"
